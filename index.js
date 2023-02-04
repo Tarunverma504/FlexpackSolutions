b179899771dc525cbb7b2615658af3c181224d2a
Tarun Verma <vermatarun4305@gmail.com>
2023-02-05 01:36:23 +0530

    completed

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const el = document.getElementById('category');
 const hiddenDiv = document.getElementById('categoryOptions');
 el.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv.style.display = 'block';
-  
-    // 👇️ if you used visibility property to hide the div
-    // hiddenDiv.style.visibility = 'visible';
   });
   
-
-  // ✅ (optionally) Hide DIV on mouse out
-  hiddenDiv.addEventListener('mouseout', function handleMouseOut() {
+hiddenDiv.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv.style.display = 'none';
-  
-    // 👇️ if you used visibility property to hide the div
-    // hiddenDiv.style.visibility = 'hidden';
   });
+
+  function toggler(){
+
+    const el = document.getElementById('category');
+    const hiddenDiv = document.getElementById('categoryOptions');
+      if(hiddenDiv.style.display == "block"){
+        
+        hiddenDiv.style.display = 'none';
+      }
+      else
+        hiddenDiv.style.display = 'block';
+  }
